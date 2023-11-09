bd3f877439d872fe0d3b954a492c2a086c139ec5
Renata Giera-Dubiel <renatagiera@gmail.com>
2023-11-09 00:16:59 +0000

    feat: implement move South and East function

--- src/rover-direction.test.ts
+++ src/rover-direction.test.ts
@@ -1,8 +1,11 @@
 import { directionN } from "./rover-directions";
+import { directionS } from "./rover-directions";
+import { directionE } from "./rover-directions";
+//import { directionW } from "./rover-directions";
 import { Position } from './rover-initial-position';
 import { position } from './rover-initial-position';
 describe("Mars rover moves to North", () => {
-  test("Moving first step towards north (M) - (0, 1) N", () => {
+  test("Moving first step towards North (M): (0, 1) N", () => {
     const NewPosition: Position = {
       x: 0,
       y: 1,
@@ -10,7 +13,7 @@ describe("Mars rover moves to North", () => {
     };
     expect(directionN('M', position)).toEqual(NewPosition);
   });
-  test("Turning right (R) - (1, 0) E", () => {
+  test("Turning right when heading North (R): (1, 0) E", () => {
     const position1New: Position = {
       x: 1,
       y: 0,
@@ -20,3 +23,43 @@ describe("Mars rover moves to North", () => {
   });
 });
 
+describe("Mars rover moves to South", () => {
+  test("Moving first step towards South (M): (0, -1) S", () => {
+    const NewPosition: Position = {
+      x: 0,
+      y: -1,
+      d: 'S',
+    };
+    expect(directionS('M', position)).toEqual(NewPosition);
+  });
+  test("Turning right (R) when heading South: (1, 0) E", () => {
+    const position2New: Position = {
+      x: 1,
+      y: 0,
+      d: 'E',
+    };
+    expect(directionS('R', position)).toEqual(position2New);
+  });
+
+});
+
+describe("Mars rover moves to East", () => {
+  test("Moving first step towards East (M): (1,0) E", () => {
+    const NewPosition3: Position = {
+      x: 1,
+      y: 0,
+      d: 'E',
+    };
+    expect(directionE('M', position)).toEqual(NewPosition3);
+  });
+  test("Turning right (R) when heading East (1, 0) E", () => {
+    const position4New: Position = {
+      x: 0,
+      y: -1,
+      d: 'S',
+    };
+    expect(directionE('R', position)).toEqual(position4New);
+  });
+
+
+});
--- src/rover-directions.ts
+++ src/rover-directions.ts
@@ -13,4 +13,30 @@ export const directionN = (instruction: string, currentPosition: Position): Posi
   }
   return currentPosition;
 };
+//  ↓
+export const directionS = (instruction: string, currentPosition: Position): Position => {
+  if (instruction === 'M') {
+    return { x: currentPosition.x, y: currentPosition.y - 1, d: 'S' };
+  }
+  if (instruction === 'L') {
+    return { x: currentPosition.x - 1, y: currentPosition.y, d: 'W' };
+  }
+  if (instruction === 'R') {
+    return {x: currentPosition.x + 1, y: currentPosition.y, d: 'E' };
+  }
+  return currentPosition;
+};
 
+// →
+export const directionE = (instruction: string, currentPosition: Position): Position => {
+  if (instruction === 'M') {
+    return {x: currentPosition.x + 1, y: currentPosition.y, d: 'E' };
+  }
+  if (instruction === 'L') {
+    return {x: currentPosition.x, y: currentPosition.y + 1, d: 'N' };
+  }
+  if (instruction === 'R') {
+    return {x: currentPosition.x, y: currentPosition.y - 1, d: 'S' };
+  }
+  return currentPosition;
+};
